9d5399bd25b7a8812c08deef7dedd698fbd5edce
BoanergesJunior <boanerges_junior@hotmail.com>
2024-04-28 17:15:17 -0300

    nodejs with mysql dockerfile

--- docker-full-cycle/node/index.js
+++ docker-full-cycle/node/index.js
@@ -1,8 +1,22 @@
 const express = require('express');
 
+const mysql = require('mysql');
 const app = express();
 
 const port = 3000;
+
+const config = {
+  host: 'db',
+  user: 'root',
+  password: 'root',
+  database: 'nodedb',
+};
+
+const connection = mysql.createConnection(config);
+
+const sql = `INSERT INTO people(name) values ('Boanerges')`;
+connection.query(sql);
+connection.end();
 
 app.get('/', (req, res) => {
   return res.send('Hello World');
